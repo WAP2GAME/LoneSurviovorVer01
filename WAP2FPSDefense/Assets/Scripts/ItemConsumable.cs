22edcb5d50b8231fd414833d01dde6933a1deae0
haeinsa <wo4014@gmail.com>
2020-11-25 16:10:42 +0900

    [Item] 아이템 클래스 추가
    #details
    1. 아이템베이스 클래스 추가, 스크립터블 오브젝트 상속
    2. 아이템 소모품 클래스 추가, 아이템베이스 상속
    3. 아이템 플레이어 강화 추가, 아이템베이스 상속

---
+++ WAP2FPSDefense/Assets/Scripts/ItemConsumable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class ItemConsumable : ItemBase
+{
+    [SerializeField]
+    protected float baseCoolTime;
+    [SerializeField]
+    protected int retainCntLimit;
+    bool isOnCoolTime;
+
+    public float BaseCoolTime
+    {
+        get => baseCoolTime;
+    }
+    public int RetainCntLimit
+    {
+        get => retainCntLimit;
+    }
+
+    public abstract void ExecuteItemEffect();
+    protected IEnumerator CountCoolTime()
+    {
+        if(isOnCoolTime)
+            yield break;
+
+        isOnCoolTime = true;
+        float second = 0;
+        while(second< baseCoolTime)
+            second++;
+        isOnCoolTime = false;
+    }
+}
