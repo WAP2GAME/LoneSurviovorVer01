0ea8fdeea5a5ac5d88e4f921e29509c5284ed9eb
haeinsa <wo4014@gmail.com>
2020-11-28 19:53:54 +0900

    [StageInfoContainer] 스테이지 정보를 담는 스크립터블 오브젝트 파생 클래스 추가

---
+++ WAP2FPSDefense/Assets/Scripts/StageInfoContainer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StageInfo",menuName = "Scriptable Object/Stage Info")]
+class StageInfoContainer : ScriptableObject
+{
+    [SerializeField]
+    private Vector3 playerSpawnPos;
+    [SerializeField]
+    private Vector3 defendObjSpawnPos;
+
+    [SerializeField]
+    private List<Vector3> enemySpawnPosList = new List<Vector3>(10);
+
+    [SerializeField]
+    private float requireSurviveTime = 30f;
+    private bool isFinished = false;
+
+    public Vector3 PlayerSpawnPos
+    {
+        get => playerSpawnPos;
+    }
+    public Vector3 DefendObjSpawnPos
+    {
+        get => defendObjSpawnPos;
+    }
+    public ReadOnlyCollection<Vector3> EnemySpawnPosList
+    {
+        get => enemySpawnPosList.AsReadOnly();
+    }
+    public float RequireSurviveTime
+    {
+        get => requireSurviveTime;
+    }
+    public bool IsFinished
+    {
+        get;
+        set;
+    }
+}
+
