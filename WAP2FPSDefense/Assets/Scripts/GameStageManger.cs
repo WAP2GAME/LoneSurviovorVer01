bbb03a5bf7605b9cd1bc7d8ce647e5e87496b812
haeinsa <wo4014@gmail.com>
2020-11-29 20:08:51 +0900

    [GameStageManager] 스테이지 이동을 관리하는 매니저 클래스 추가

---
+++ WAP2FPSDefense/Assets/Scripts/GameStageManger.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStageManger : MonoSingleton<GameStageManger>
+{
+    [SerializeField]
+    private List<StageInfoContainer> stageList = new List<StageInfoContainer>();
+    [SerializeField]
+    private GameObject playerObj = null;
+    public int FinishedStageCnt
+    {
+        private set;
+        get;
+    }
+    public StageInfoContainer CurrentStage
+    {
+        private set;
+        get;
+    }
+
+    public void MoveStage()
+    {
+
+    }
+
+    private void SetObjectPositionsOf(StageInfoContainer stage)
+    {
+        playerObj.transform.position = stage.PlayerSpawnPos;
+    }
+
+    private StageInfoContainer GetRandomStage()
+    {
+        if (++FinishedStageCnt >= stageList.Count)
+            EnableAllStage();
+
+        StageInfoContainer stage = null;
+        while(stage == null)
+        {
+            int idx = Random.Range(0, stageList.Count);
+            if (!stageList[idx].IsFinished)
+                stage = stageList[idx];
+        }
+
+        stage.IsFinished = true;
+        return stage;
+    }
+
+    private void EnableAllStage()
+    {
+        foreach (var stage in stageList)
+            stage.IsFinished = false;
+    }
+}
