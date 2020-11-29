9d7c65259a9fec323c111bc016121e859ccd8aeb
haeinsa <wo4014@gmail.com>
2020-11-29 20:06:33 +0900

    [Stat] 체력을 관리하는 스탯 클래스 추가, 및 적 부위별 피격 스크립트 추가

---
+++ WAP2FPSDefense/Assets/Scripts/EnemyStat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public enum EBodyParts
+{
+    Head,
+    Leg,
+    Arm,
+    Body
+}
+class EnemyStat : ObjectStat
+{
+    [SerializeField]
+    private Stat overallStat;
+    [SerializeField]
+    private Dictionary<EBodyParts, Stat> bodyPartStats = new Dictionary<EBodyParts, Stat>();
+
+    public override void TakeDamage(float damage, EBodyParts hitParts)
+    {
+        base.TakeDamage(damage);
+        Stat hitPart = bodyPartStats[hitParts];
+        hitPart.TakeDamage(damage);
+
+        AffectBodyInjury(hitParts);
+    }
+
+    private void AffectBodyInjury(EBodyParts part)
+    {
+
+    }
+}
+
