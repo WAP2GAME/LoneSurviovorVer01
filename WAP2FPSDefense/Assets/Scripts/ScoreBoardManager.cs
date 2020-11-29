fe7d029e9b238f7a4fce4e609b8d8db5a78a5031
haeinsa <wo4014@gmail.com>
2020-11-29 20:05:46 +0900

    [ScoreBoardManager] 스테이지 종료 후 점수 UI를 제공하는 스크립트 추가

---
+++ WAP2FPSDefense/Assets/Scripts/ScoreBoardManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoardManager : MonoBehaviour
+{
+    [Header("Buttons")]
+    [SerializeField]
+    private Button exitBtn = null;
+    [SerializeField]
+    private Button shopBtn = null;
+    [SerializeField]
+    private Button stageJumpBtn = null;
+
+    [Header("Scroe Show")]
+    [SerializeField]
+    private Text surviveTimeScore = null;
+    [SerializeField]
+    private Text killCountScore = null;
+    [SerializeField]
+    private Text resultText = null;
+
+
+    public void SetScore(float surviveTime, float killCnt)
+    {
+        surviveTimeScore.text = surviveTime.ToString();
+        killCountScore.text = killCnt.ToString();
+    }
+
+    public void SetResultAsWon(bool isWon)
+    {
+        if (isWon)
+            resultText.text = "Congraturation";
+        else
+            resultText.text = "Game Over";
+        shopBtn.gameObject.SetActive(isWon);
+        stageJumpBtn.gameObject.SetActive(isWon);
+    }
+}
