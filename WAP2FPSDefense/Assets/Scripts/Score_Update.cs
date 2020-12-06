9f38d7783e8aa112cb7aac1adf2d8fdb3f8a25b2
DESKTOP-QHM548V\user <dkrm56@naver.com>
2020-12-06 18:55:49 +0900

    Revert "Coin_Update + Score_Manager + Score_Update => ScoreManager 통합"
    This reverts commit 5dc8eabbdd6e71fd1a978461454207219ff3c49d.

---
+++ WAP2FPSDefense/Assets/Scripts/Score_Update.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score_Update : MonoBehaviour
+{
+    Text scoreLabel;
+
+    void Awake()
+    {
+        scoreLabel = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        scoreLabel.text = "Score: " + Score_Manager.score.ToString();
+    }
+
+}
