b11ef5e9df44b810df62482d97ee5dab29e24be0
DESKTOP-QHM548V\user <dkrm56@naver.com>
2020-12-06 21:37:30 +0900

    [ScoreManager] 수정본

---
+++ WAP2FPSDefense/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+    public static int coin;
+
+    public Text scoreText;
+    public Text coinText;
+
+    void Start()
+    {
+        score = 0;
+        coin = 0;
+    }
+
+    void Update()
+    {
+        scoreText.text = "Score: " + score;
+        coinText.text = "Coin: " + coin;
+
+    }
+}
