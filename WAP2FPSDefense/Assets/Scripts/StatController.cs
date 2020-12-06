432f37aa44442bad2c9ea54e2a26616012947a30
DESKTOP-PU7CUSJ\동민 <mysummer321@gmail.com>
2020-12-06 13:21:55 +0900

    [inventory, slot, statcontroller] script 추가

---
+++ WAP2FPSDefense/Assets/Scripts/StatController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatController : MonoBehaviour
+{
+
+    [SerializeField]
+    private ObjectStat theObjectStat;
+
+    [SerializeField]
+    private Image[] imagestat;
+
+    private Stat stat;
+   
+
+    void Update()
+    {
+        CheckStat();
+    }
+
+    private void CheckStat()
+    {
+        float Healthrate = theObjectStat.HealthPoint / theObjectStat.HealthPointMax;
+        imagestat[0].fillAmount = Healthrate;
+    }
+
+    void OnEnable()
+    {
+        theObjectStat = GetComponent<ObjectStat>();
+    }
+}
