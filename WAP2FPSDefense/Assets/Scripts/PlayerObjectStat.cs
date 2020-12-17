ce242b13ebc5d57c8bd3df9e746767fd860dce60
haeinsa <wo4014@gmail.com>
2020-12-17 16:26:53 +0900

    [PlayeObjectStat] 스테이지 종료 노티파이어를 구현한 플레이어 스탯 클래스 추가

---
+++ WAP2FPSDefense/Assets/Scripts/PlayerObjectStat.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+class PlayerObjectStat : ObjectStat , IStageEndNotifier 
+{
+    private List<IStageEndObserver> stageEndObservers = new List<IStageEndObserver>();
+    private bool IsGameOver(float damage)
+    {
+        return stat.HealthPoint - damage <= 0;
+    }
+    public override void TakeDamage(float damage)
+    {
+        if (IsGameOver(damage))
+        {
+            Notify();
+            return;
+        }
+        base.TakeDamage(damage);
+    }
+
+    public void Notify()
+    {
+        foreach (var a in stageEndObservers)
+            a.EndStage();
+    }
+
+    public bool AddObserver(IStageEndObserver observer)
+    {
+        if (stageEndObservers.Contains(observer))
+            return false;
+        stageEndObservers.Add(observer);
+        return true;
+    }
+
+    public void DeleteObserver(IStageEndObserver observer)
+    {
+
+    }
+
+    protected new void OnEnable()
+    {
+        base.OnEnable();
+        var observer = FindObjectsOfType(typeof(IStageEndObserver));
+        foreach (var a in observer)
+            if(a is IStageEndObserver)
+               stageEndObservers.Add(a as IStageEndObserver);
+    }
+}
+
