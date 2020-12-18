8aee95b4aae81e7b23ffbc0532b7325deab3e917
haeinsa <wo4014@gmail.com>
2020-12-18 19:43:54 +0900

    [Slot] 슬롯 클래스 로우 클래스로 다운

--- WAP2FPSDefense/Assets/Scripts/Slot.cs
+++ WAP2FPSDefense/Assets/Scripts/Slot.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Slot : MonoBehaviour
+[Serializable]
+public class Slot
 {
 
     public ItemBase item; // 획득한 아이템.
