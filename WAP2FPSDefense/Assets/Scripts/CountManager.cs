12d49f786f00399180c3793c98617fdf26e6050b
pakryonghyun <en5796@naver.com>
2020-12-19 16:51:52 +0900

    [ZombieManager , CountManager] 스크립트 추가 (좀비매니저 : 좀비강화 , 카운트매니저 : 시간표기)

---
+++ WAP2FPSDefense/Assets/Scripts/CountManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static System.Math;
+public class CountManager : MonoBehaviour
+{
+    //private CountManager countManager;
+    public static CountManager Instance = null;
+    public static CountManager instance
+    {
+        get
+        {
+            return Instance;
+        }
+    }
+    [SerializeField] StageInfoContainer stageinfocontainer;
+    [SerializeField] Text PlayCountText;
+    public float Count = 0;
+    [SerializeField] Text RequireCountText;
+    private float RequireTime;
+
+    void Awake()
+    {
+        //countManager = CountManager.Instance;
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+
+        RequireTime = stageinfocontainer.RequireSurviveTime;
+    }
+    public void FixedUpdate()
+    {
+        RequireTime -= Time.deltaTime;
+        RequireCountText.text = "RequireTime :" + "" + string.Format("{0:0.00}",Mathf.Round((float)RequireTime*100)*0.01f) + ""; //string.Format으로 소수점 둘째자리까지 고정해서 fix
+
+        if (RequireTime < 0)
+        {
+            RequireTime = 30;
+        }
+        Count += Time.deltaTime;
+        PlayCountText.text = "playTime : " + (int)Count + "";
+
+    }
+}
---
+++ WAP2FPSDefense/Assets/Scripts/ZombieManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ZombieManager :MonoBehaviour
+{ 
+    //private ZombieManager zombieManger;
+    private static ZombieManager Instance = null;
+    public GameObject Player;
+    public GameObject Nexus;
+    public static ZombieManager instance
+    {
+        get
+        {
+            return Instance;
+        }
+    }
+    private void Awake()
+    {
+        //zombieManger = ZombieManager.Instance;
+        if ( Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        ZombieCount = GetComponent<CountManager>().Count;
+    }
+    public int AddZombieHP;
+    public int AddZombiePower;
+    float ZombieCount;
+    IEnumerator zombieEhance()
+    {
+        ZombieCount += Time.deltaTime;
+        if (ZombieCount > 10)
+        {
+            ZombieCount = 0;
+            Debug.Log("zombie Enhance");
+            AddZombieHP += 100;
+            AddZombiePower += 10;
+            ZombieCount += Time.deltaTime;
+        }
+        yield return new WaitForSecondsRealtime(10);
+    }
+    public void FixedUpdate()
+    {
+        StartCoroutine(zombieEhance());
+    }
+    public void ZombieReset()
+    {
+        AddZombieHP = 0;
+        AddZombiePower = 0;
+    }
+}
