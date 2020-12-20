43a9900dc87d8f9603d2daccaaa70548e9ecf931
haeinsa <wo4014@gmail.com>
2020-12-20 17:58:00 +0900

    [StageFlowManager] 추가
    #details
    1.카운터매니저 => 스테이지타임카운터로 변경
    2. 스테이지타임카운터에 현재 스테이지 탈출가능여부 알림 기능 추가
    3. 현재 스테이지의 흐름을 관리하는 스테이지 플로우 모노싱글톤 클래스 추가

---
+++ WAP2FPSDefense/Assets/Scripts/StageFlowManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class StageFlowManager : MonoSingleton<StageFlowManager> , IStageEndNotifier ,IStageChangeObserver
+{
+    [SerializeField]
+    private StageTimeCounter timeCounter;
+    private List<IStageEndObserver> stageEndObservers = new List<IStageEndObserver>();
+
+    public float RequireTime
+    {
+        get => timeCounter.RequireTime;
+    }
+    public float Count
+    {
+        get => timeCounter.Count;
+    }
+
+    public void NotifyEnd()
+    {
+        timeCounter.EndStage();
+    }
+
+    public void ChangeStage(StageInfoContainer stage)
+    {
+        timeCounter.ChangeStage(stage);
+        foreach (var a in stageEndObservers)
+            if(a != null)
+               a.EndStage();
+    }
+
+    private void EscapeStage()
+    {
+        if (timeCounter.RequireTime <= 0 && Input.GetKeyDown(KeyCode.E))
+            NotifyEnd();
+    }
+
+    public void Update()
+    {
+        timeCounter.UpdateTime();
+        EscapeStage();
+    }
+
+    private void Awake()
+    {
+        var observers = FindObjectsOfType(typeof(IStageEndObserver));
+        foreach (var a in observers)
+            if (a is IStageEndObserver)
+                stageEndObservers.Add(a as IStageEndObserver);
+    }
+}
