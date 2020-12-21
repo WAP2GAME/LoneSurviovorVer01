a29302afb52ba09587e012c8f9b07a4e76b1bc85
haeinsa <wo4014@gmail.com>
2020-12-21 22:31:47 +0900

    [StageTimeUI] 스테이지 시간을 표시해주눈 UI 매니저 추가

---
+++ WAP2FPSDefense/Assets/Scripts/UI/StageTimeUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class StageTimeUI : MonoBehaviour 
+{
+    [SerializeField]
+    private Text playCountText;
+    [SerializeField]
+    private Text requireCountText;
+    [SerializeField]
+    private Text escpaeInformText = null;
+
+    public void RefreshTimeUI()
+    {
+        if (StageFlowManager.Instance.IsOnStage)
+        {
+            var requireTime = StageFlowManager.Instance.RequireTime;
+            var count = StageFlowManager.Instance.Count;
+            requireCountText.text = "RequireTime :" + "" + string.Format("{0:0.00}", Mathf.Round((float)requireTime * 100) * 0.01f) + "";
+            playCountText.text = "playTime : " + (int)count + "";
+            escpaeInformText.gameObject.SetActive(requireTime <= 0);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        RefreshTimeUI();
+    }
+}
