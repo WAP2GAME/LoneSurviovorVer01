1c0840696d4ba0eb0c7ea25424b565274cba3d48
haeinsa <wo4014@gmail.com>
2020-12-23 10:52:24 +0900

    [ItemContainer] 인게임 아이템 정보를 담는 데이터베이스 클래스 추가

---
+++ WAP2FPSDefense/Assets/Scripts/ItemContainer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemContainer : MonoSingleton<ItemContainer>
+{
+    [SerializeField]
+    private List<ItemBase> items = new List<ItemBase>(50);
+
+    public List<ItemBase> Items
+    {
+        get
+        {
+            return new List<ItemBase>(items);
+        }
+    }
+
+    public List<ItemConsumable> ItemConsumables
+    {
+        get
+        {
+            List<ItemConsumable> consumables = new List<ItemConsumable>();
+            foreach(var a in items)
+                if (a.ItemType == EItemType.consumable && a is ItemConsumable)
+                    consumables.Add(a as ItemConsumable);
+
+            return consumables;
+        }
+    }
+
+}
+
