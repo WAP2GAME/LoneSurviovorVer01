92e4e3f05dc31d3ff3cf00b48981827ca1c59dcb
haeinsa <wo4014@gmail.com>
2020-12-23 12:06:41 +0900

    no message

--- WAP2FPSDefense/Assets/Scripts/SpawnManager.cs
+++ WAP2FPSDefense/Assets/Scripts/SpawnManager.cs
@@ -26,6 +26,7 @@ public class SpawnManager : MonoBehaviour, IStageChangeObserver, IStageEndObserv
 
     public void ChangeStage(StageInfoContainer stage)
     {
+        stageEndSpawn = false;
         spawnpos.Clear();
         var newList = stage.EnemySpawnPosList;
         for (int q = 0; q < newList.Count; q++)
