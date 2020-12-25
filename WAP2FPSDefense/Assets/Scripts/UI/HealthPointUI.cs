c3c67b2cba4223751e9b389765048e1c6dfdb10c
haeinsa <wo4014@gmail.com>
2020-12-25 11:03:31 +0900

    [HealthPointUI] 체력바 인터페이스 관리 클래스 추가

---
+++ WAP2FPSDefense/Assets/Scripts/UI/HealthPointUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthPointUI : MonoBehaviour
+{
+    [SerializeField]
+    private ObjectStat targetStat;
+    private Image healthBar;
+
+    private void Update()
+    {
+        var ratio = targetStat.HealthPoint / targetStat.HealthPointMax;
+        healthBar.fillAmount = ratio;
+    }
+    private void Awake()
+    {
+        healthBar = GetComponent<Image>();
+    }
+}
