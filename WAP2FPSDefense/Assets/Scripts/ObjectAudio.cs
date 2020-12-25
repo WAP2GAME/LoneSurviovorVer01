0603b06e459d41453599877f6b66b068904d4f0b
haeinsa <wo4014@gmail.com>
2020-12-25 10:42:25 +0900

    [ObjectAudio] 어디오소스 컴포넌트를 관리하는 래핑 클래스 추가

---
+++ WAP2FPSDefense/Assets/Scripts/ObjectAudio.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+
+[RequireComponent(typeof(AudioSource))]
+public class ObjectAudio : MonoBehaviour
+{
+    [SerializeField]
+    protected List<AudioClip> auidos;
+    [SerializeField]
+    protected AudioSource audioSource;
+    public bool IsPlayeing
+    {
+        protected set { IsPlayeing = value; }
+        get => audioSource.isPlaying;
+    }
+
+    public virtual void Play(int idx)
+    {
+        if (idx >= auidos.Count)
+            return;
+
+        if (IsPlayeing)
+            audioSource.Stop();
+        audioSource.clip = auidos[idx];
+        audioSource.Play();
+    }
+
+    public virtual void Play()
+    {
+        if (audioSource.clip != null)
+            audioSource.Play();
+    }
+
+    public void Stop()
+    {
+        audioSource.Stop();
+    }
+
+    protected virtual void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+}
+
